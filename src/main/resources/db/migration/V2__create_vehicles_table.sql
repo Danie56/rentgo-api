bce28b7742d7c11ba5f1f18908e8dec758b72999
daniel <danisanto0179@gmail.com>
2026-06-06 15:51:21 -0500

    add migration file for vehicle

---
+++ src/main/resources/db/migration/V2__create_vehicles_table.sql
@@ -0,0 +1,21 @@
+CREATE TABLE vehicles (
+                          id UUID PRIMARY KEY,
+
+                          plate VARCHAR(20) NOT NULL UNIQUE,
+
+                          brand VARCHAR(100) NOT NULL,
+
+                          model VARCHAR(100) NOT NULL,
+
+                          year INTEGER NOT NULL,
+
+                          color VARCHAR(50) NOT NULL,
+
+                          daily_rate NUMERIC(10,2) NOT NULL,
+
+                          status VARCHAR(20) NOT NULL,
+
+                          created_at TIMESTAMP NOT NULL,
+
+                          updated_at TIMESTAMP NULL
+);
